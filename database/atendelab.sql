979535f10445576b1352040649a2ca6f3a4a914e
TrizFEy <166865532+TrizFEy@users.noreply.github.com>
2026-06-09 21:07:37 -0300

    Update atendelab.sql

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -1,3 +1,52 @@
+CREATE DATABASE IF NOT EXISTS atendelab
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_general_ci;
+USE atendelab;
+
+CREATE TABLE `usuarios` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `email` varchar(100) UNIQUE NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `perfil` enum('admin','aluno', 'atendente') DEFAULT 'atendente',
+  `status` enum('ativo','inativo') DEFAULT 'ativo',
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `pessoas` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `documento` varchar(20) NOT NULL UNIQUE,
+  `telefone` varchar(20) NOT NULL,
+  `curso` varchar(100) NOT NULL,
+  `periodo` varchar(100) NOT NULL,
+  `status` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `tipos_atendimentos` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  `descricao` TEXT NOT NULL,
+  `status` ENUM('ativo', 'inativo') DEFAULT 'ativo'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `atendimentos` (
+  `id` int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  `pessoa_id` int(11), 
+  `tipo_atendimento_id` int(11) NOT NULL,
+  `usuario_id` int(11),
+  `data_atendimento` DATE NOT NULL DEFAULT (CURRENT_DATE),
+  `hora_atendimento` TIME NOT NULL DEFAULT (CURRENT_TIME),
+  `descricao` TEXT NOT NULL,
+  `observacao` TEXT,
+  `status` ENUM('ativo', 'inativo') DEFAULT 'ativo',
+  `criado_em` timestamp NOT NULL DEFAULT current_timestamp(),
+  CONSTRAINT `fk_atendimentos_pessoas` FOREIGN KEY (`pessoa_id`) REFERENCES `pessoas` (`id`) ON DELETE SET NULL,
+  CONSTRAINT `fk_atendimentos_tipos` FOREIGN KEY (`tipo_atendimento_id`) REFERENCES `tipos_atendimentos` (`id`),
+  CONSTRAINT `fk_atendimentos_usuarios` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`) ON DELETE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 INSERT INTO usuarios (nome, email, senha, perfil, status)
 VALUES (
  'Administrador',
